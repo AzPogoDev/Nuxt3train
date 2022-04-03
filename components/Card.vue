a522103ffdcc243ca91b9de48c1fbcccd3fba6d6
AzPogoDev <henricousin07@gmail.Com>
2022-04-03 15:29:11 +0200

    Initial commit

---
+++ components/Card.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Card</h1>
+      <iframe src='https://my.spline.design/weirdbubblecopy-b8315e930fe69a0c616520ecbae71739/' frameborder='0' width='100%' height='100%'></iframe> 
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <h1>Nuxt 3 starter template with all folders and hint</h1>
+  <Card></Card>
 </template>
 
 <script setup>
+import Card from '~~/components/Card.vue';
 </script>
 
 <style lang="scss" scoped>
